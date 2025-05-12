9bde0fe92954166dc50487576fce9d22e473c278
Nadun Sameera Bandara <bandarans2000@gmail.com>
2025-05-12 18:10:26 +0530

    install laravel broadcasting and create event and config pusher from backend

--- resources/frontend/src/components/navbar/Navbar.jsx
+++ resources/frontend/src/components/navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
                                 alt='Flowbite Logo'
                             />
                             <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
-                                Flowbite
+                                nadun Dev
                             </span>
                         </a>
                         <div className='flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse'>
